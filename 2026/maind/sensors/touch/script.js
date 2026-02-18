0dae9dacf29394b77b41cbd516051bc1eb03faa9
Leonardo Angelucci <dev@unstated.co>
2026-02-18 23:19:45 +0100

    Update script.js

--- 2026/maind/sensors/touch/script.js
+++ 2026/maind/sensors/touch/script.js
@@ -8,10 +8,17 @@ function update(e) {
   const t = e.touches; // all current touches on the screen
   let text = `fingers: ${t.length}<br />`;
 
+  // loop through the touches and add the x and y coordinates to the text
   for (let i = 0; i < t.length; i++) {
     text += `#${i + 1}: x=${Math.round(t[i].clientX)} y=${Math.round(t[i].clientY)}<br />`;
   }
 
+  if (t.length > 3) {
+   document.body.style.backgroundColor = "black";
+  } else {
+    document.body.style.backgroundColor = "white";
+  }
+  // update the text in the info div
   info.innerHTML = text.trim();
 }
 
